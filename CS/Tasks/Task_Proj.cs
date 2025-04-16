8a2d3b824adb4892c1a817deda8c0a004c886ec1
marcin kuczok <mkuczok6@gmail.com>
2025-04-16 20:29:35 +0200

    Dodaj pliki projektów.

---
+++ CS/Tasks/Task_Proj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDAppProject.CS.Tasks
+{
+    internal class Task_Proj
+    {
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDAppProject
+{
+    class CRUDAppProject
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+}
